f893c61e4ebd78091afb36f0c29509d711820b9a
01knappe <36029090+01knappe@users.noreply.github.com>
2020-11-13 16:35:54 +0100

    0.7

---
+++ data/js/index.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    if ($('#inline').length) {
+        $('#inline').minicolors({
+            inline: $('#inline').attr('data-inline') === 'true',
+            theme: 'bootstrap'
+        });
+    }
+});
+
+function setColor() {
+    var color = $('#inline').minicolors('rgbObject');
+    console.log(color);
+    $.post("/color", color);
+}
+
+var brightness = document.getElementById("rangeBrightness");
+      var output = document.getElementById("demo");
+      output.innerHTML = brightness.value;
+  
+      brightness.oninput = function() {
+        output.innerHTML = this.value;
+      }
+
+function setBrightness() {
+    var jas = $('#rangeBrightness').val();
+    console.log(jas);
+    $.post("/brightness", { brightness: jas });
+}
+
+setInterval(function () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("time").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "/time", true);
+    xhttp.send();
+  }, 1000);
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,27 @@
 #include <FastLED.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPAsyncTCP.h>
+#include <SPI.h>
+#include <FS.h>
 
-#define NUM_LEDS 7    
+#define NUM_LEDS 30    
 #define DATA_PIN D6
 CRGB leds[NUM_LEDS];
-CRGB color = CRGB::Red; 
+//CRGB color = CRGB::Red; 
+byte r_val = 255;
+byte g_val = 0;
+byte b_val = 0;
 
-const char *ssid     = "ssid";
-const char *password = "password";
+CRGB color = CRGB(r_val, g_val, b_val);
+
+const char *ssid     = "Knappe_Home";
+const char *password = "Odysea1347";
 
 AsyncWebServer server(80);
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org");
 
-byte brightness = 255;
+byte brightness = 100;
 
 String getTime() {
   String time = timeClient.getFormattedTime();
@@ -47,12 +54,39 @@ void setup() {
     request->send(SPIFFS, "/index.html", String());
   });
 
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/css/style.css", String());
+  });
+
   server.on("/time", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", getTime().c_str());
   });
 
   server.on("/brightness", HTTP_POST, [](AsyncWebServerRequest *request) {    
     brightness = request->arg("brightness").toInt();    
+    request->send_P(200, "text/json", "{\"result\":\"ok\"}");
+  });
+
+  server.on("/jquery.minicolors.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/css/jquery.minicolors.css", String());
+  });
+
+  server.on("/index.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/js/index.js", String());
+  });
+
+  server.on("/jquery.minicolors.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/js/jquery.minicolors.min.js", String());
+  });
+
+  server.on("/jquery.minicolors.png", HTTP_GET, [](AsyncWebServerRequest *request){
+  request->send(SPIFFS, "/css/jquery.minicolors.png", "image/png");
+  });
+
+   server.on("/color", HTTP_POST, [](AsyncWebServerRequest *request) {    
+    r_val = request->arg("r").toInt();
+    g_val = request->arg("g").toInt();
+    b_val = request->arg("b").toInt();
     request->send_P(200, "text/json", "{\"result\":\"ok\"}");
   });
 
@@ -80,13 +114,13 @@ void loop() {
   
   FastLED.setBrightness(brightness);
 //test ledek
-  for (int i = 0; i <= 6; i++) {
+  for (int i = 0; i <= 29; i++) {
     leds[i] = CRGB ( 0, 0, 255);
     FastLED.show();
     delay(40);
   }
-  for (int i = 6; i >= 0; i--) {
-    leds[i] = CRGB ( 255, 0, 0);
+  for (int i = 29; i >= 0; i--) {
+    leds[i] = CRGB(r_val, g_val, b_val);
     FastLED.show();
     delay(40);
   }
