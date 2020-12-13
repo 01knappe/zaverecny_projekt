1b09835d28383c9234c14ac73f6b19c1ccea5177
01knappe <36029090+01knappe@users.noreply.github.com>
2020-12-13 16:16:41 +0100

    1.2

--- data/js/index.js
+++ data/js/index.js
@@ -8,6 +8,63 @@ $(document).ready(function () {
     }
 });
 
+var mode = 0;
+
+function showClock() {
+    $.post("/clock");
+    mode = 0;    
+    console.log(mode);
+}
+
+function showTemperature() {
+    $.post("/temperature");
+    mode = 1;
+    console.log(mode);
+}
+
+function showHumidity() {
+    $.post("/humidity");
+    mode = 2;
+    console.log(mode);
+}
+
+setInterval(function () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+          if(mode == 0){
+            document.getElementById("time").innerHTML = this.responseText;
+          }
+          else if(mode == 1){
+              if(this.responseText==2147483647){
+                document.getElementById("time").innerHTML = "-- °C";
+              }  
+              else{
+                document.getElementById("time").innerHTML = this.responseText+" °C";
+                }          
+        }
+        else if(mode == 2){
+            if(this.responseText==2147483647){
+                document.getElementById("time").innerHTML = "-- %";
+            }
+            document.getElementById("time").innerHTML = this.responseText+" %";
+        }          
+      }
+    };
+    if(mode == 0){
+        xhttp.open("GET", "/time", true);
+        xhttp.send();
+    }
+    else if(mode == 1){
+        xhttp.open("GET", "/teplota", true);
+        xhttp.send();
+    }
+    else if(mode == 2){
+        xhttp.open("GET", "/vlhkost", true);
+        xhttp.send();
+    }   
+}, 1000);
+
 function setColor() {
     var color = $('#inline').minicolors('rgbObject');
     var pcolor = $('#inline').minicolors('rgbString');
@@ -30,35 +87,18 @@ function setBrightness() {
     $.post("/brightness", { brightness: jas });
 }
 
-function showClock() {
-    $.post("/clock");
-}
-
-function showTemperature() {
-    $.post("/temperature");
-}
-
-function showHumidity() {
-    $.post("/humidity");
-}
-
-setInterval(function () {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("time").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "/time", true);
-    xhttp.send();
-  }, 1000);
-
 setInterval(function () {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("teplota").innerHTML = this.responseText;
-        }
+            if(this.responseText==2147483647){
+                document.getElementById("teplota").innerHTML = "-- °C";
+              }
+              else{
+                document.getElementById("teplota").innerHTML = this.responseText+" °C";
+                }
+    
+            }
     };
     xhttp.open("GET", "/teplota", true);
     xhttp.send();
@@ -68,8 +108,15 @@ setInterval(function () {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("vlhkost").innerHTML = this.responseText;
-      }
+          if(this.responseText==2147483647){
+            document.getElementById("vlhkost").innerHTML = "-- %";
+          }
+          else{
+            document.getElementById("vlhkost").innerHTML = this.responseText+" %";
+            }
+
+          }
+       
     };
     xhttp.open("GET", "/vlhkost", true);
     xhttp.send();
